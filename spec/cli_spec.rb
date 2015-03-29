b99fffc1387085b4efb2f68b9786c46fe3f78772
seo <seotownsend@icloud.com>
2015-03-29 09:55:35 -0400

    Added new spec, removed mistaken image

--- spec/cli_spec.rb
+++ spec/cli_spec.rb
@@ -2,8 +2,13 @@ require './lib/flok.rb'
 require 'tempfile'
 require 'securerandom'
 
+def ensure_tmp
+  tmp_spec_path = './spec/tmp'
+  Dir.mkdir(tmp_spec_path) unless File.exists?(tmp_spec_path)
+end
+
 RSpec.describe "CLI" do
-  it "Creates a new module folder" do
+  it "Creates a new module folder with absolute path" do
     #Get a temporary file, delete it, but save the path
     temp = Tempfile.new "flok-temp"
     path = temp.path
@@ -13,5 +18,14 @@ RSpec.describe "CLI" do
     `ruby -Ilib ./bin/flok new #{path}`
 
     (Dir.exists? path).should eq(true)
+  end
+
+  it "Creates a new module folder with relative path" do
+    ensure_tmp
+    fn = SecureRandom.hex
+
+    dir = "./spec/tmp/#{fn}"
+    `ruby -Ilib ./bin/flok new #{dir}`
+    (File.exists?(dir).should eq(true))
   end
 end
