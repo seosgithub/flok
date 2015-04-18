05287cab8941ef7e298f15f039cbfd319349fedc
seo <seotownsend@icloud.com>
2015-04-18 11:29:49 -0400

    tests

--- spec/build_valid_spec.rb
+++ spec/build_valid_spec.rb
@@ -10,17 +10,33 @@ def build_world_for_platform platform
 end
 
 RSpec.describe "Emitted build products are valid for all platforms" do
-  it "Can run application.js on execjs environment without runtime initial failure" do
-    #Get a list of the platforms based on the drev folder names
+  it "can run application.js on execjs environment without runtime initial failure" do
+    #get a list of the platforms based on the drev folder names
     platforms = (Dir["./app/drivers/*"]).map!{|e| File.basename(e)} - ["iface"]
 
     platforms.each do |p|
-      puts "Testing #{p}"
-      puts "\t-Building world for $PLATFORM=#{p}"
+      puts "testing #{p}"
+      puts "\t-building world for $platform=#{p}"
       build_world_for_platform(p)
 
-      puts "\t-Executing world"
+      puts "\t-executing world"
       ctx = ExecJS.compile(File.read("./products/#{p}/application.js"))
     end
   end
+
+  it "can run application.js and supports IFACES" do
+    #get a list of the platforms based on the drev folder names
+    platforms = (Dir["./app/drivers/*"]).map!{|e| File.basename(e)} - ["iface"]
+
+    platforms.each do |p|
+      puts "testing #{p}"
+      puts "\t-building world for $platform=#{p}"
+      build_world_for_platform(p)
+
+      puts "\t-executing world"
+      ctx = ExecJS.compile(File.read("./products/#{p}/application.js"))
+      ifaces = ctx.eval("IFACES")
+    end
+  end
+
 end
