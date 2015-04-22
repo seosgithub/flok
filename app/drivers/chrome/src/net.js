f638deb3a0e91006771e88aa00654194c058d753
seo <seotownsend@icloud.com>
2015-04-21 22:24:33 -0400

    update

--- app/drivers/chrome/src/net.js
+++ app/drivers/chrome/src/net.js
@@ -7,16 +7,13 @@ if_net_request = function(verb, url, params) {
   if_net_request_socket_index += 1
   fd = if_net_request_socket_index
 
-  system.stderr.writeLine("zzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz")
   $.ajax({url: url, type: verb, data: params}).success(function(data) {
-      system.stderr.writeLine("+++++++++++++++++++++++++++++++++++++++++++")
       //int_net_callback(0, 0, 0);
       //data = JSON.parse(data);
     }).error(function(xhr, status, err) {
-      system.stderr.writeLine("+++++++++++++++++++++++++++++++++++++++++++")
       console.log("he")
-    }).always(function(e) {
-      system.stderr.writeLine("------------------------------------")
+    }
+    enuth
     })
 
 
