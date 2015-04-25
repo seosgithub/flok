a5d591ad3c8dacc652f92a755793cb837ca2bc9b
seo <seotownsend@icloud.com>
2015-04-25 10:12:43 -0400

    Updated net.js in chrome driver to support telepointers

--- app/drivers/chrome/src/net.js
+++ app/drivers/chrome/src/net.js
@@ -2,7 +2,7 @@
 var if_net_request_socket_index = 0
 
 //A basic get request that supports callbacks
-if_net_req = function(verb, url, params) {
+if_net_req = function(verb, url, params, tp_base) {
   console.error(url)
   $.ajax({
     url: url,
@@ -10,7 +10,7 @@ if_net_req = function(verb, url, params) {
     data: params,
     success: function(data) {
       data = JSON.parse(data);
-      int_dispatch([3, "int_net_cb", true, data]);
+      int_dispatch([3, "int_net_cb", true, data, tp_base]);
     },
     error: function(xhr, textStatus, err) {
       console.error(err);
