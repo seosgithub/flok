641767d4b00a85495564ee740bb2e1befed08713
seo <seotownsend@icloud.com>
2015-04-25 19:33:02 -0400

    net fix

--- spec/kern/net_spec.rb
+++ spec/kern/net_spec.rb
@@ -31,6 +31,7 @@ RSpec.describe "kern:net_spec" do
     expect(@info).to eq({"secret" => @secret})
   end
 
+  #Careful, therubyracer has some deadlock issues
   it "get_req() will not respond if the owner no longer exists" do
     @secret = SecureRandom.hex
    
@@ -38,19 +39,11 @@ RSpec.describe "kern:net_spec" do
       @info = info.to_h
     end
 
-    q = Queue.new
     #External loopback
     external_function "if_net_req" do |verb, url, params, tp_base|
       Thread.new do
-        begin
-          q.push 1
-          q.pop
-          external_int "int_net_cb", tp_base, true, {"secret" => @secret}
-          puts "here"
-          q.push 1
-        rescue Exception => e
-          $stderr.puts e
-        end
+        sleep 1
+        external_int "int_net_cb", tp_base, true, {"secret" => @secret}
       end
     end
     
@@ -65,11 +58,36 @@ RSpec.describe "kern:net_spec" do
     @ctx.eval %{
       tel_del(owner);
     }
-
-    q.pop
-    q.push 1
-    q.pop
+    sleep 2
 
     expect(@info).to eq(nil)
+  end
+
+  #Guard false positive of no-owner w/ timing
+  it "get_req() will respond if the owner still exists" do
+    @secret = SecureRandom.hex
+   
+    function "test_complete" do |info|
+      @info = info.to_h
+    end
+
+    #External loopback
+    external_function "if_net_req" do |verb, url, params, tp_base|
+      Thread.new do
+        sleep 1
+        external_int "int_net_cb", tp_base, true, {"secret" => @secret}
+      end
+    end
+    
+    puts @ctx.eval %{
+      owner = tel_reg(1)
+      get_req(owner, "http://test_url", {}, function(info) {
+        test_complete(info);
+      });
+    }
+
+    sleep 2
+
+    expect(@info).to eq({"secret"=>@secret})
   end
 end
