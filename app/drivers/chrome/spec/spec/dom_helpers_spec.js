c5614a11e51356132b773e1af25ec18d1408cd80
seo <seotownsend@icloud.com>
2015-05-23 11:10:09 -0400

    Added button emit test for CHROME

--- app/drivers/chrome/spec/spec/dom_helpers_spec.js
+++ app/drivers/chrome/spec/spec/dom_helpers_spec.js
@@ -55,7 +55,7 @@ QUnit.test("When the DOM bind data-puts is used with 'src foo' in a controller t
   assert.equal(_src, src, "src is supposed to be a URL: " + src + ", but instead got: " + _src);
 });
 
-QUnit.test("When a button is clicked with the data-emit tag, it will signal the controller", function(assert) {
+QUnit.test("When a button is clicked with the data-emit tag, it will signal the flok controller", function(assert) {
   $("body").html("                                            \
     <div id='prototypes'>                                     \
       <div class='view' data-name='my_test_view'>            \
@@ -66,37 +66,26 @@ QUnit.test("When a button is clicked with the data-emit tag, it will signal the 
     </div>                                                    \
   ")      
 
-  //Controller that will set the variable foo to button clicked when the action 'button_clicked' is received
-  var TestController = function() {
-    this.base = FlokController; this.base(); var self = this;
-
-    self.init = function() {
-      //You have access to 'context' in here
-    }
-
-    self.action = function(from, to) {
-    }
-
-    self.event = function(name, info) {
-      if (name === "button_clicked") {
-        foo = "button_clicked";
-      }
-    }
-  }
-
-  //Register the controller
-  regController("__test__", TestController);
-
-  //Create an instance of our prototype
+  //Create an instance of our prototype view
   if_init_view("my_test_view", {}, 4, ["main"]);
 
-  //Bind the controller to our view and set the bp to 3
+  //Bind a generic FlokController to our view and set the bp to 3
+  //It's generic because __test__ does not exist
   if_controller_init(3, 4, "__test__", {});
 
   //Attach that view's controller to the root spot
   if_attach_view(4, 0);
 
+  //Grab the view's button
   var $button = $("#root #trigger_button_clicked");
+
+  //Intercept click event
+  window.int_dispatch = function(q) {
+    assert.equal(JSON.stringify(q), JSON.stringify([3, "int_event", 3, "button_clicked", {}]));
+  }
+
+  //Trigger button
   $button.trigger("click");
-  assert.equal(foo, "button_clicked", "Button was supposed to trigger a clicked event, but it did not");
+
+  window.int_dispatch = null;
 });
