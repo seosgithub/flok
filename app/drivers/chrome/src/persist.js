4316a168fe7ba6633f571cfd59360c5600e1d98e
seo <seotownsend@icloud.com>
2015-05-27 15:52:07 -0400

    Added docs and base specs for persist

---
+++ app/drivers/chrome/src/persist.js
@@ -0,0 +1,14 @@
+function if_per_set(ns, key, value) {
+}
+
+function if_per_del(ns, key) {
+}
+
+function if_per_del_ns(ns) {
+}
+
+function if_per_get(s, ns, key) {
+}
+
+function if_per_get_sync(s, ns, key) {
+}
---
+++ spec/iface/driver/persist_spec.rb
@@ -0,0 +1,22 @@
+Dir.chdir File.join File.dirname(__FILE__), '../../../'
+require './spec/env/iface.rb'
+require './spec/lib/helpers.rb'
+require './spec/lib/io_extensions.rb'
+require './spec/lib/rspec_extensions.rb'
+
+RSpec.describe "iface:driver:persist" do
+  module_dep "persist"
+  include_context "iface:driver"
+
+  it "Can receive persist API messages without crashing" do
+    #Disk is scheduling class 2
+    @pipe.puts [[2, 3, "if_per_set", "my_ns", "my_key", "my_value"]].to_json
+    @pipe.puts [[2, 3, "if_per_get", "session", "my_ns", "my_key"]].to_json
+    @pipe.puts [[2, 3, "if_per_get_sync", "session", "my_ns", "my_key"]].to_json
+    @pipe.puts [[2, 2, "if_per_del", "my_ns", "my_key"]].to_json
+    @pipe.puts [[2, 1, "if_per_del_ns", "my_ns"]].to_json
+
+    #Wait for response
+    @pipe.puts [[0, 0, "ping"]].to_json; @pipe.readline_timeout
+  end
+end
