7ab0eec6ae26d1fb8bdcd8451768abde9599b608
seo <seotownsend@icloud.com>
2015-05-27 15:54:59 -0400

    Fixed persist spec and added flush to docs

--- spec/iface/driver/persist_spec.rb
+++ spec/iface/driver/persist_spec.rb
@@ -12,9 +12,10 @@ RSpec.describe "iface:driver:persist" do
     #Disk is scheduling class 2
     @pipe.puts [[2, 3, "if_per_set", "my_ns", "my_key", "my_value"]].to_json
     @pipe.puts [[2, 3, "if_per_get", "session", "my_ns", "my_key"]].to_json
-    @pipe.puts [[2, 3, "if_per_get_sync", "session", "my_ns", "my_key"]].to_json
+    @pipe.puts [[0, 3, "if_per_get_sync", "session", "my_ns", "my_key"]].to_json
     @pipe.puts [[2, 2, "if_per_del", "my_ns", "my_key"]].to_json
     @pipe.puts [[2, 1, "if_per_del_ns", "my_ns"]].to_json
+    @pipe.puts [[0, 0, "if_per_flush_sync"]].to_json
 
     #Wait for response
     @pipe.puts [[0, 0, "ping"]].to_json; @pipe.readline_timeout
