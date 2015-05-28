a06bddb9db72ec36e885cccba85792103dcdc0ff
seo <seotownsend@icloud.com>
2015-05-28 13:10:15 -0400

    Forgot files

--- app/drivers/chrome/src/persist.js
+++ app/drivers/chrome/src/persist.js
@@ -1,18 +1,29 @@
 function if_per_set(ns, key, value) {
-  localStorage.setItem(ns+"___"+key, value);
+  var _ns = store.namespace(ns);
+  _ns.set(key, value)
 }
 
 function if_per_del(ns, key) {
-  localStorage.removeItem(ns+"___"+key);
+  var _ns = store.namespace(ns);
+  _ns.remove(key);
 }
 
 function if_per_del_ns(ns) {
+  var _ns = store.namespace(ns);
+  _ns.clearAll();
 }
 
 function if_per_get(s, ns, key) {
+  function async() {
+    var _ns = store.namespace(ns);
+    var res = _ns.get(key);
+    int_dispatch([2, "int_per_get_res", s, res]);
+  }
+  setTimeout(async, 0);
 }
 
 function if_per_get_sync(s, ns, key) {
-  var res = localStorage.getItem(ns+"___"+key);
+  var _ns = store.namespace(ns);
+  var res = _ns.get(key);
   int_dispatch([2, "int_per_get_res", s, res]);
 }
