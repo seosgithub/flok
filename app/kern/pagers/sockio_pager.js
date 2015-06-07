199b32400f1b3ba67bd93798f6123caf00fe1e53
seo <seotownsend@icloud.com>
2015-06-07 19:21:22 -0400

    gem test

--- app/kern/pagers/sockio_pager.js
+++ app/kern/pagers/sockio_pager.js
@@ -1,6 +1,16 @@
 <% if @defines['sockio_pager'] %>
   sockio_pager_data = {};
-  //SEND(1, 2, "if_sockio_init", localhost")
+
+  sockio_pager_sp = null;
+  sockio_pager_bp = null;
+  waiting_bp = null;
+  function sockio_pager_init(options) {
+    sockio_pager_sp = tels(1);
+    sockio_pager_bp = tels(1);
+    SEND("net", "if_sockio_init", options.url, sockio_pager_sp);
+    reg_evt(sockio_pager_bp, sockio_pager_sp_endpoint);
+    SEND("net", "if_sockio_fwd", sockio_pager_sp, "user", sockio_pager_bp);
+  }
 
   function sockio_pager_read_sync(ns, bp, key) {
     throw "sockio_pager does not support read_sync"
@@ -12,11 +22,15 @@
       value: sockio_pager_data[key],
     }
 
-    int_event(bp, "read_res", info);
-    vm_cache[ns][key] = sockio_pager_data[key];
+    waiting_bp = bp;
   }
 
   function sockio_pager_write(key, value) {
     throw "sockio_pager does not support write"
+  }
+
+  //Where socket.io data comes in
+  function sockio_pager_sp_endpoint(bp, ename, info) {
+    int_event(waiting_bp, "read_res", info);
   }
 <% end %>
