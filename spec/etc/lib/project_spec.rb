f4354420611113d86b0c12065c845bab35f36932
seo <seotownsend@icloud.com>
2015-06-07 12:51:09 -0400

    uncommented spec

--- spec/etc/lib/project_spec.rb
+++ spec/etc/lib/project_spec.rb
@@ -3,25 +3,25 @@ require './lib/flok'
 require './spec/env/etc'
 
 RSpec.describe "lib/project" do
-  #it "can list project_template files" do
-    #ls = Flok::Project.list
-    ##Subject to change but it's just a basic test
-    #expect(ls).to include("Gemfile")
-    #expect(ls).to include("app/controllers")
-  #end
+  it "can list project_template files" do
+    ls = Flok::Project.list
+    #Subject to change but it's just a basic test
+    expect(ls).to include("Gemfile")
+    expect(ls).to include("app/controllers")
+  end
 
-  #it "can create project_template files" do
-    #dir = new_temp_dir
-    #Dir.chdir dir do
-      #Flok::Project.create "test"
-      #Dir.chdir "test" do
-        ##This is subject to change, but it's just a basic test
-        #expect(dirs).to include("app")
-        #expect(dirs).to include("app/controllers")
-        #expect(files).to include("Gemfile")
-      #end
-    #end
-  #end
+  it "can create project_template files" do
+    dir = new_temp_dir
+    Dir.chdir dir do
+      Flok::Project.create "test"
+      Dir.chdir "test" do
+        #This is subject to change, but it's just a basic test
+        expect(dirs).to include("app")
+        expect(dirs).to include("app/controllers")
+        expect(files).to include("Gemfile")
+      end
+    end
+  end
 
   it "Does contain a copy of the config.yml for the currently active platform in ./config/platforms/$PLATFORM/config.yml" do
     platform = ENV['PLATFORM']
