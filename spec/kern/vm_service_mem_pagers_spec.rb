ff89ea0282dd951f5f5f5bb25a13283e70b9f30c
seo <seotownsend@icloud.com>
2015-06-15 09:53:28 -0400

    mem spec pagers

--- spec/kern/vm_service_mem_pagers_spec.rb
+++ spec/kern/vm_service_mem_pagers_spec.rb
@@ -11,92 +11,92 @@ RSpec.describe "kern:vm_service_mem_pagers" do
   include Zlib
   include_context "kern"
 
-  #it "Can initialize the pg_mem0 pager" do
-    #ctx = flok_new_user File.read('./spec/kern/assets/vm/controller0.rb'), File.read("./spec/kern/assets/vm/pg_mem/config.rb") 
-    #ctx.eval %{
-      #//Call embed on main root view
-      #base = _embed("my_controller", 0, {}, null);
+  it "Can initialize the pg_mem0 pager" do
+    ctx = flok_new_user File.read('./spec/kern/assets/vm/controller0.rb'), File.read("./spec/kern/assets/vm/pg_mem/config.rb") 
+    ctx.eval %{
+      //Call embed on main root view
+      base = _embed("my_controller", 0, {}, null);
 
-      #//Drain queue
-      #int_dispatch([]);
-    #}
+      //Drain queue
+      int_dispatch([]);
+    }
 
-    #res = ctx.eval("pg_mem0_spec_did_init")
-    #expect(res).to eq(true)
-  #end
+    res = ctx.eval("pg_mem0_spec_did_init")
+    expect(res).to eq(true)
+  end
 
-  #it "Can make a write request to pg_mem0 and have that written in cache" do
-    #ctx = flok_new_user File.read('./spec/kern/assets/vm/pg_mem/write.rb'), File.read("./spec/kern/assets/vm/pg_mem/config.rb") 
-    #ctx.eval %{
-      #//Call embed on main root view
-      #base = _embed("my_controller", 0, {}, null);
+  it "Can make a write request to pg_mem0 and have that written in cache" do
+    ctx = flok_new_user File.read('./spec/kern/assets/vm/pg_mem/write.rb'), File.read("./spec/kern/assets/vm/pg_mem/config.rb") 
+    ctx.eval %{
+      //Call embed on main root view
+      base = _embed("my_controller", 0, {}, null);
 
-      #//Drain queue
-      #int_dispatch([]);
-    #}
+      //Drain queue
+      int_dispatch([]);
+    }
 
-    #page = ctx.dump "page"
-    #vm_cache = ctx.dump("vm_cache")
-    #expect(page).to eq(vm_cache["local"]["test"])
-  #end
+    page = ctx.dump "page"
+    vm_cache = ctx.dump("vm_cache")
+    expect(page).to eq(vm_cache["local"]["test"])
+  end
 
-  #it "Can initialize the pg_mem1 pager" do
-    #ctx = flok_new_user File.read('./spec/kern/assets/vm/controller0.rb'), File.read("./spec/kern/assets/vm/pg_mem/config1.rb") 
-    #ctx.eval %{
-      #//Call embed on main root view
-      #base = _embed("my_controller", 0, {}, null);
+  it "Can initialize the pg_mem1 pager" do
+    ctx = flok_new_user File.read('./spec/kern/assets/vm/controller0.rb'), File.read("./spec/kern/assets/vm/pg_mem/config1.rb") 
+    ctx.eval %{
+      //Call embed on main root view
+      base = _embed("my_controller", 0, {}, null);
 
-      #//Drain queue
-      #int_dispatch([]);
-    #}
+      //Drain queue
+      int_dispatch([]);
+    }
 
-    #res = ctx.eval("pg_mem1_spec_did_init")
-    #expect(res).to eq(true)
-  #end
+    res = ctx.eval("pg_mem1_spec_did_init")
+    expect(res).to eq(true)
+  end
 
-  #it "Can make a write request to pg_mem1 and have that written in cache" do
-    #ctx = flok_new_user File.read('./spec/kern/assets/vm/pg_mem/write.rb'), File.read("./spec/kern/assets/vm/pg_mem/config1.rb") 
-    #ctx.eval %{
-      #//Call embed on main root view
-      #base = _embed("my_controller", 0, {}, null);
+  it "Can make a write request to pg_mem1 and have that written in cache" do
+    ctx = flok_new_user File.read('./spec/kern/assets/vm/pg_mem/write.rb'), File.read("./spec/kern/assets/vm/pg_mem/config1.rb") 
+    ctx.eval %{
+      //Call embed on main root view
+      base = _embed("my_controller", 0, {}, null);
 
-      #//Drain queue
-      #int_dispatch([]);
-    #}
+      //Drain queue
+      int_dispatch([]);
+    }
 
-    #page = ctx.dump "page"
-    #vm_cache = ctx.dump("vm_cache")
-    #expect(page).to eq(vm_cache["local"]["test"])
-  #end
+    page = ctx.dump "page"
+    vm_cache = ctx.dump("vm_cache")
+    expect(page).to eq(vm_cache["local"]["test"])
+  end
 
-  #it "Can initialize the pg_mem2 pager" do
-    #ctx = flok_new_user File.read('./spec/kern/assets/vm/controller0.rb'), File.read("./spec/kern/assets/vm/pg_mem/config2.rb") 
-    #ctx.eval %{
-      #//Call embed on main root view
-      #base = _embed("my_controller", 0, {}, null);
+  it "Can initialize the pg_mem2 pager" do
+    ctx = flok_new_user File.read('./spec/kern/assets/vm/controller0.rb'), File.read("./spec/kern/assets/vm/pg_mem/config2.rb") 
+    ctx.eval %{
+      //Call embed on main root view
+      base = _embed("my_controller", 0, {}, null);
 
-      #//Drain queue
-      #int_dispatch([]);
-    #}
+      //Drain queue
+      int_dispatch([]);
+    }
 
-    #res = ctx.eval("pg_mem2_spec_did_init")
-    #expect(res).to eq(true)
-  #end
+    res = ctx.eval("pg_mem2_spec_did_init")
+    expect(res).to eq(true)
+  end
 
-  #it "Can make a write request to pg_mem2 and have that written in cache" do
-    #ctx = flok_new_user File.read('./spec/kern/assets/vm/pg_mem/write.rb'), File.read("./spec/kern/assets/vm/pg_mem/config2.rb") 
-    #ctx.eval %{
-      #//Call embed on main root view
-      #base = _embed("my_controller", 0, {}, null);
+  it "Can make a write request to pg_mem2 and have that written in cache" do
+    ctx = flok_new_user File.read('./spec/kern/assets/vm/pg_mem/write.rb'), File.read("./spec/kern/assets/vm/pg_mem/config2.rb") 
+    ctx.eval %{
+      //Call embed on main root view
+      base = _embed("my_controller", 0, {}, null);
 
-      #//Drain queue
-      #int_dispatch([]);
-    #}
+      //Drain queue
+      int_dispatch([]);
+    }
 
-    #page = ctx.dump "page"
-    #vm_cache = ctx.dump("vm_cache")
-    #expect(page).to eq(vm_cache["local"]["test"])
-  #end
+    page = ctx.dump "page"
+    vm_cache = ctx.dump("vm_cache")
+    expect(page).to eq(vm_cache["local"]["test"])
+  end
 
   it "Can use pg_mem0 and pg_mem1 at the same time" do
     ctx = flok_new_user File.read('./spec/kern/assets/vm/pg_mem/write2.rb'), File.read("./spec/kern/assets/vm/pg_mem/config3.rb") 
@@ -109,8 +109,9 @@ RSpec.describe "kern:vm_service_mem_pagers" do
     }
 
     page = ctx.dump "page"
+    page2 = ctx.dump "page2"
     vm_cache = ctx.dump("vm_cache")
     expect(page).to eq(vm_cache["local0"]["test"])
-    expect(page).to eq(vm_cache["local1"]["test"])
+    expect(page2).to eq(vm_cache["local1"]["test"])
   end
 end
