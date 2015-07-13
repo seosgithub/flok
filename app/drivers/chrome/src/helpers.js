2ff2ed51f2688f5f24ae702a7afc7c223da415b7
Seo Townsend <seotownsend@icloud.com>
2015-07-13 19:10:55 -0400

    Removed case sensetive incorrectly cased duplicate

--- app/drivers/chrome/src/helpers.js
+++
@@ -1,6 +0,0 @@
-//Get a pseudo UUID
-uuid_counter = 0;
-UUID = function() {
-  uuid_counter += 1;
-  return uuid_counter;
-}
--- app/drivers/chrome/src/timer.js
+++
@@ -1,9 +0,0 @@
-function timer_callback() {
-  //Call timer interrupt
-  int_dispatch([0, "int_timer"]);
-}
-
-function if_timer_init(tps) {
-  //Call timer n times per second
-  setInterval(timer_callback, 1.0/tps*1000.0);
-}
