c190cbebf5fd6b1039d20ea4e6c5a5b04700a8df
seo <seotownsend@icloud.com>
2015-10-21 13:58:06 -0400

    updated example pod dependencies

--- app/drivers/apple/flok-pod/Example/Pods/Headers/Private/Nimble/NMBExceptionCapture.h
+++ app/drivers/apple/flok-pod/Example/Pods/Headers/Private/Nimble/NMBExceptionCapture.h
@@ -1,8 +1,1 @@
-#import <Foundation/Foundation.h>
-
-@interface NMBExceptionCapture : NSObject
-
-- (id)initWithHandler:(void(^)(NSException *))handler finally:(void(^)())finally;
-- (void)tryBlock:(void(^)())unsafeBlock;
-
-@end
+../../../Nimble/Nimble/objc/NMBExceptionCapture.h
