9f4a33a193673bbbb4f9dac34ba21ff034b5f148
Seo Townsend <seotownsend@icloud.com>
2016-01-26 14:38:37 -0500

    require relative

--- lib/flok.rb
+++ lib/flok.rb
@@ -1,15 +1,15 @@
-require "flok/macro"
-require "flok/version"
-require "flok/utilities"
-require "flok/build"
-require "flok/platform"
-require "flok/project"
+require_relative "flok/macro"
+require_relative "flok/version"
+require_relative "flok/utilities"
+require_relative "flok/build"
+require_relative "flok/platform"
+require_relative "flok/project"
 
-require "flok/interactive"
-require "flok/user_compiler"
-require "flok/services_compiler"
-require "flok/transition_compiler"
-require "flok/hooks_compiler"
+require_relative "flok/interactive"
+require_relative "flok/user_compiler"
+require_relative "flok/services_compiler"
+require_relative "flok/transition_compiler"
+require_relative "flok/hooks_compiler"
 
 module Flok
 end
